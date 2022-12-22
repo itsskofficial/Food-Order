2362a7c331de47ac599b639a6d65c564192e668b
SK <sarthakkarandikar03@gmail.com>
2022-12-22 05:29:54 +0000

    Dec 22, 2022, 10:59 AM

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -11,7 +11,7 @@ const Header = (props) => {
                 <h1>
                     Food Order
                 </h1>
-                <HeaderCartButton>
+                <HeaderCartButton/>
                 </HeaderCartButton>
             </header>
             <div className={classes["main-image"]}>
