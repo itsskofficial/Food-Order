b6cdb63301d72a4fbff7b0f53f7f96fba3bcbce1
SK <sarthakkarandikar03@gmail.com>
2022-12-22 05:00:15 +0000

    Dec 22, 2022, 10:30 AM

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,10 +3,13 @@ import { Fragment } from "react"
 const Header = (props) => {
     return (
         <Fragment>
+            <header>
             <h1>
                 Food Order
             </h1>
+            </header>
             
+
         </Fragment>
     )
 }
