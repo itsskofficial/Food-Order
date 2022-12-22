431dd57794334c40cdc444096898d152be4aa94d
SK <sarthakkarandikar03@gmail.com>
2022-12-22 05:15:44 +0000

    Dec 22, 2022, 10:45 AM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import { Fragment } from "react"
+import Header from "./components/Layout/Header"
+
 const App = () => {
   return (
-    
+    <Fragment>
+      <Header></Header>
+    </Fragment>
   )
 }
 
