e7ef90faa011526f0c241c6af1a4d208f9bf7785
SK <sarthakkarandikar03@gmail.com>
2022-12-22 05:48:46 +0000

    Dec 22, 2022, 11:18 AM

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,9 +1,10 @@
 import { Fragment } from "react"
+import MealsSummary from "./MealsSummary"
 
 const Meals = () => {
     return (
         <Fragment>
-            
+            <MealsSummary></MealsSummary>
         </Fragment>
     )
 }
