14f5eebcd6688a26f0010251c94358650db5dfc1
SK <sarthakkarandikar03@gmail.com>
2022-12-26 10:01:18 +0000

    Dec 26, 2022, 3:31 PM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import classes from "./css/Cart.module.css"
 
 const Cart = (props) => {
     const cartItems = <ul>
-        {[id:1,name:"Burger",]}
+        {[id:1 ,name:"Burger", amount: 2, price: "12.99"]}
     </ul>
     return (
         <div>
