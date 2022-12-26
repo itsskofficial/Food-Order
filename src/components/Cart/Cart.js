6f1e8ef50f8226f5f964db3384728ddac7920be5
SK <sarthakkarandikar03@gmail.com>
2022-12-26 10:00:16 +0000

    Dec 26, 2022, 3:30 PM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,12 @@
 import classes from "./css/Cart.module.css"
 
 const Cart = (props) => {
+    const cartItems = <ul>
+        
+    </ul>
     return (
         <div>
-            
+
         </div>
     )
 }
