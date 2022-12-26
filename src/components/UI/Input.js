45a4540640f1db76d58fc54b3e357d0e8d25e648
SK <sarthakkarandikar03@gmail.com>
2022-12-26 08:52:19 +0000

    Dec 26, 2022, 2:22 PM

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,7 +3,7 @@ import classes from "./css/Input.module.css"
 const Input = (props) => {
     return (
         <div className={classes.input}>
-            <label htmlFor={classes.input.id}>
+            <label htmlFor={props.input.id}>
                 {props.label}
             </label>
             <input {...props.input}/>
