1a9d1aec3394675e0a5fc0572eee07ddfaefe064
SK <sarthakkarandikar03@gmail.com>
2022-12-26 08:26:42 +0000

    Dec 26, 2022, 1:56 PM

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -4,6 +4,8 @@ const Input = (props) => {
     return (
         <div className={classes.input}>
             <label htmlFor={classes.input.id}>
+                {props.label}
+            </label>
         </div>
     )
 }
