c546004e3472135c771278aecd72ccbebc36c25c
SK <sarthakkarandikar03@gmail.com>
2022-12-26 08:37:08 +0000

    Dec 26, 2022, 2:07 PM

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,7 +4,7 @@ import Input from "../../UI/Input"
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-            <Input label="/>
+            <Input label="Amount" input={{}} />
             <Button type="submit">
                 Add Item
             </Button>
