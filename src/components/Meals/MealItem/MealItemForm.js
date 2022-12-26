8434bfdd6c98ccf7ccd54e7803316f068c8061bc
SK <sarthakkarandikar03@gmail.com>
2022-12-26 08:18:17 +0000

    Dec 26, 2022, 1:48 PM

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,7 +2,9 @@ import classes from "./css/MealItemForm.js"
 
 const MealItemForm = (props) => {
     return (
-        
+        <form className={classes.form}>
+            <in
+        </form>
     )
 }
 
