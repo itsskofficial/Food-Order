4c7aff3326fa626a69dd46d72baa1cf4befdb223
SK <sarthakkarandikar03@gmail.com>
2022-12-26 11:09:47 +0000

    Dec 26, 2022, 4:39 PM

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
                 <h1>
                     Food Order
                 </h1>
-                <HeaderCartButton onClick = {props./>
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes["main-image"]}>
                 <img alt="Food" src={foodImage} />
