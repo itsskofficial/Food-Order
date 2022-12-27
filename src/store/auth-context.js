d1d643a08682456590badf601a597d6e2de72f78
SK <sarthakkarandikar03@gmail.com>
2022-12-27 07:42:36 +0000

    Dec 27, 2022, 1:12 PM

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,6 +1,6 @@
 import { React, useEffect, useState } from 'react'
 
-const AuthContext = React.createContext({
+const CartContext = React.createContext({
     items: [],
     totalAmount: 0,
     addItem : (item) => { },
