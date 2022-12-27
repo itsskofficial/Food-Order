a642f93a25deac15c5e51336579601d47281a794
SK <sarthakkarandikar03@gmail.com>
2022-12-27 07:41:41 +0000

    Dec 27, 2022, 1:11 PM

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -3,7 +3,7 @@ import { React, useEffect, useState } from 'react'
 const AuthContext = React.createContext({
     items: [],
     totalAmount: 0,
-    
+    addItem = (item) => {}
 })
 
 const AuthContextProvider = (props) => {
