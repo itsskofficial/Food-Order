5d3abc2b4b380525c47d5180fd5bb91eb21d431a
SK <sarthakkarandikar03@gmail.com>
2022-12-27 07:46:52 +0000

    Dec 27, 2022, 1:16 PM

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -3,7 +3,7 @@ import CartContext from "./cart-context"
 const CartProvider = (props) => {
     return (
         <CartContext.Provider>
-            {}
+            {props.children}
         </CartContext.Provider>
     )
 }
