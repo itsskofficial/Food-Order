512e1426d948bec15741745997399bd52ce595bb
SK <sarthakkarandikar03@gmail.com>
2023-01-01 13:04:24 +0000

    Jan 1, 2023, 6:34 PM

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,13 +2,14 @@ import { useContext } from "react"
 import CartContext from "../../store/cart-context"
 import Button from "../UI/Button"
 import Modal from "../UI/Modal"
+import CartItem from "./CartItem"
 import classes from "./css/Cart.module.css"
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const cartItems = <ul className={classes['cart-items']}>
-        {cartCtx.items.map((item)=><li>{item.name}</li>)}
+        {cartCtx.items.map((item)=><li><CartItem</li>)}
     </ul>
     return (
         <Modal onClose={props.onClose}>
