7edda1aefd97fa32d3ee8d348fd12bf34ef62d11
SK <sarthakkarandikar03@gmail.com>
2023-01-01 12:41:46 +0000

    Jan 1, 2023, 6:11 PM

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -11,7 +11,7 @@ const MealItem = (props) => {
             id: props.id,
             name: props.name,
             amount: amount,
-            price
+            price:props.price
         })
     }
     return (
