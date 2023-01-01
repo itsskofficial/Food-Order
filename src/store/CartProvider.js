81489d6559055fb43df9882c7cf9babb2d2653e2
SK <sarthakkarandikar03@gmail.com>
2023-01-01 13:31:02 +0000

    Jan 1, 2023, 7:01 PM

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -24,7 +24,7 @@ const cartReducer = (state, action) => {
         
         else {
             updatedItem = { ...action.item }
-            updatedItems = state.items.concat(action.item)
+            updatedItems = state.items.concat(up))
         }
         
         const updatedTotalAmount = state.totalAmount + state.item.price * state.item.amount
