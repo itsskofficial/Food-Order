5d3cf769076fa7bcdaee468550ce10067d7628d3
SK <sarthakkarandikar03@gmail.com>
2023-01-01 13:21:48 +0000

    Jan 1, 2023, 6:51 PM

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,7 +8,7 @@ const defaultCartState = {
     
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const existingItemsIndex = 
+        const existingItemsIndex = state.items.findIndex(item=> stat)
         const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + state.item.price * state.item.amount
         return ({
