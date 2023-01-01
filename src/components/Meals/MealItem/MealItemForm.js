f2f8d96885031158a40c5be29d1f6b3b85354cf1
SK <sarthakkarandikar03@gmail.com>
2023-01-01 12:31:05 +0000

    Jan 1, 2023, 6:01 PM

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,7 +4,7 @@ import Input from "../../UI/Input"
 import Button from "../../UI/Button"
 
 const MealItemForm = (props) => {
-    const [amountIsValid,setAmountIsValid] = useState()
+    const [amountIsValid,setAmountIsValid] = useState(true)
     const amountInputRef = useRef()
     const submitEventHandler = (event) => {
         event.preventDefault()
