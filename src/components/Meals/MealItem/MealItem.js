32a1bddc6621353d7c0d67c20c6a8669bad73711
SK <sarthakkarandikar03@gmail.com>
2023-01-01 12:39:41 +0000

    Jan 1, 2023, 6:09 PM

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -3,6 +3,7 @@ import classes from "./css/MealItem.module.css"
 import MealItemForm from "./MealItemForm"
 
 const MealItem = (props) => {
+    const cartCtx
     const price = `$ ${props.price.toFixed(2)}`
     const addToCartHandler = (amount) => {
 
