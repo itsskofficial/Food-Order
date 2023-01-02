9f377a57c44695a03cd6f51aa1182bb47e044315
SK <sarthakkarandikar03@gmail.com>
2023-01-02 00:23:07 +0000

    Jan 2, 2023, 5:53 AM

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -38,7 +38,7 @@ const cartReducer = (state, action) => {
     if (action.type === 'REMOVE') {
         const existingItemIndex = state.items.findIndex(item => item.id === action.item.id)
         const existingItem = state.items[existingItemIndex]
-        const updatedTotalAmount = state.tot
+        const updatedTotalAmount = state.totalAmount - action.item.price
     }
 }
 
