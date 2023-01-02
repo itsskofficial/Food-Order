f6fc207c6f03d2a5d99156726d11717e23f3d739
SK <sarthakkarandikar03@gmail.com>
2023-01-02 00:27:39 +0000

    Jan 2, 2023, 5:57 AM

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -36,7 +36,7 @@ const cartReducer = (state, action) => {
     }
     
     if (action.type === 'REMOVE') {
-        const existingItemIndex = state.items.findIndex(item => item.id === action.item.id)
+        const existingItemIndex = state.items.findIndex(item => item.id === action.id)
         const existingItem = state.items[existingItemIndex]
         const updatedTotalAmount = state.totalAmount - action.item.price
     }
