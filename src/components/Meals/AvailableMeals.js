5bca203daf4f030a5f62e8dd1ee8f1c98115c987
SK <sarthakkarandikar03@gmail.com>
2023-02-01 01:15:20 +0000

    Feb 1, 2023, 6:45 AM

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,7 +7,7 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   useEffect(() => {
-    const fetchMeals=async 
+    const fetchMeals=async ()
   },[])
   const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description ={meal.description} price={meal.price}/>)
     return (
